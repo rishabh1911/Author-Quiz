a6ba473dff78c0a27c507f7b16fee1011a3c4fcf
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-09 22:42:06 +0530

    BootStrap added and App.js renamed to Author.js

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+     <div> Author Quiz </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
