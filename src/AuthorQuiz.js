b4a2d94c56379485673c0e5c87658693d80354c9
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-15 00:22:09 +0530

    Added Structured components with props

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
@@ -13,24 +13,46 @@ function Hero() {
   );
 }
 
-function Turn() {
-  return (<div/>);
+function Turn({author, books}) {
+  return (
+    <div className="row turn" style={{backgroundColor: "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author"/>
+      </div>
+      <div className="col-6">
+        {books.map( (title) => <p>{title}</p> )}
+      </div>
+    </div>
+  );
 }
 
 function Continue() {
-  return (<div/>);
+  return (
+    <div className="continue"></div>
+  );
 }
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Hero />
-        <Turn />
-        <Continue /> 
+function Footer() {
+  return (
+    <div id="footer" className="row" >
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function AuthorQuiz({ turnData }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData}/>
+      <Continue />
+      <Footer />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    }
+];
+
+const state = {
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+};
+
+ReactDOM.render(<AuthorQuiz  {...state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
