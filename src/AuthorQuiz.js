0ed59243698dd7942d89a017287fe4d0bcc28b35
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-14 23:55:18 +0530

    Checking bootstrap

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 ffset-1"> 
+        <h1>Author Quiz</h1>
+        <p>Select the book written by author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Turn() {
+  return (<div/>);
+}
+
+function Continue() {
+  return (<div/>);
+}
+
 class AuthorQuiz extends Component {
   render() {
     return (
-     <div> Author Quiz </div>
+      <div className="container-fluid">
+        <Hero />
+        <Turn />
+        <Continue /> 
+      </div>
     );
   }
 }
