338a746c1017d6b524dcc8dce99cb821320649b4
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-15 19:57:10 +0530

    Cosmetic css changes

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -45,7 +45,7 @@ function Footer() {
     <div id="footer" className="row" >
       <div className="col-12">
         <p className="text-muted credit">
-          All images are from
+          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
         </p>
       </div>
     </div>
