07032b9f9c461c4ce0250ff682846292bbf35e35
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-20 00:32:08 +0530

    Add css

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./AddAuthorForm.css";
 
 class AuthorForm extends React.Component {
     constructor(props) {
@@ -41,11 +42,11 @@ class AuthorForm extends React.Component {
             <label htmlFor="name">Name</label>
             <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange}/>
         </div>
-        <div>
+        <div className="AddAuthorForm_input">
             <label htmlFor="imageUrl">Image URL</label>
             <input type="text" name="imageUrl" value={this.state.imageUrl} onChange={this.onFieldChange}/>
         </div>
-        <div>
+        <div className="AddAuthorForm_input">
             <label htmlFor="bookTemp"><h3>Books</h3></label>
             {this.state.books.map( (book) => <p key={book}>{book}</p> )}
             <input type="text" name="bookTemp" value={this.state.bookTemp} onChange={this.onFieldChange}/>
