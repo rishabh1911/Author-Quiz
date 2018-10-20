6205f3a77d20988ed0e4c0fc1d77570f00431ac2
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-10-20 15:47:21 +0530

    Changes author quiz part to React Redux - no functionality changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, withRouter } from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux'; 
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import AddAuthorForm from './AddAuthorForm';
@@ -61,28 +63,29 @@ function getTurnData(authors) {
     }
 }
 
-function resetState() {
-  return {
-    turnData:  getTurnData(authors),
-    answered: 'notAnswered'
-  };
+function reducer(state = { authors, turnData: getTurnData(authors), answered: 'notAnswered'}, action) {
+  switch (action.type){
+    case 'ANSWER_SELECTED':
+      const isCorrect = state.turnData.author.books.some( (book) => book === action.answer );
+      return Object.assign({}, state, {
+        answered : isCorrect ? 'right' : 'wrong'
+      });
+    case 'CONTINUE':
+      return Object.assign( {}, state, {
+        answered: 'notAnswered', 
+        turnData: getTurnData(state.authors)
+      });
+      default:
+        return state;
+  }
 }
 
-let state = resetState();
-
-function onAnswerSelected(answer) {
-  const isCorrect = state.turnData.author.books.some( (book) => book === answer );
-  state.answered = isCorrect ? 'right' : 'wrong';
-  render();
-}
-
-function onContinueClicked() {
-  state = resetState();
-  render();
-}
+let store = Redux.createStore(reducer);
 
 function App() {
-  return <AuthorQuiz  {...state}  onAnswerSelected={onAnswerSelected} onContinueClicked={onContinueClicked}/>;
+  return <ReactRedux.Provider store={store}>
+    <AuthorQuiz />
+  </ReactRedux.Provider>;
 }
 
 const AuthorWrapper= withRouter(({history}) =>
@@ -92,8 +95,8 @@ const AuthorWrapper= withRouter(({history}) =>
     }} />
   );
 
-function render() {
-  ReactDOM.render(
+
+ReactDOM.render(
   <BrowserRouter>
     <React.Fragment>
       <Route exact path="/"  component={App}  />
@@ -101,8 +104,7 @@ function render() {
     </React.Fragment>
   </BrowserRouter>,
   document.getElementById('root'));
-}
-render();
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
